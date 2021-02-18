609e6dade922ba70587e93a7759c7b7502aeb1f6
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2021-02-19 02:59:26 +0330

    Chnage ssid

--- android/src/main/java/com/ly/wifi/WifiDelegate.java
+++ android/src/main/java/com/ly/wifi/WifiDelegate.java
@@ -198,14 +198,13 @@ WifiDelegate implements PluginRegistry.RequestPermissionsResultListener {
                     maps.put("bssid", scanResult.BSSID);
                     maps.put("level", level);
                     list.add(maps);
+                } else {
+                    if (scanResult.BSSID.contains(key)) {
+                        maps.put("bssid", scanResult.BSSID);
+                        maps.put("level", level);
+                        list.add(maps);
                     }
-//                } else {
-//                    if (scanResult.BSSID.contains(key)) {
-//                        maps.put("bssid", scanResult.BSSID);
-//                        maps.put("level", level);
-//                        list.add(maps);
-//                    }
-               // }
+                }
             }
         }
         result.success(list);
