f08f9657ba9ffd65b1aa20f588866584d73fc946
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2021-02-19 03:01:28 +0330

    Chnage ssid

--- android/src/main/java/com/ly/wifi/WifiDelegate.java
+++ android/src/main/java/com/ly/wifi/WifiDelegate.java
@@ -109,7 +109,7 @@ WifiDelegate implements PluginRegistry.RequestPermissionsResultListener {
         }
     }
     private void launchBSSID() {
-        String wifiBSSID = wifiManager != null ? wifiManager.getConnectionInfo.getBSSID().replace("\"", "") : "";
+        String wifiBSSID = wifiManager != null ? wifiManager.getConnectionInfo.getBSSID() : "";
         if(!wifiBSSID.isEmpty()) {
             result.success(wifiBSSID);
             clearMethodCallAndResult();
